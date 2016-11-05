4414761c61ce6650ba308443b33d3e8ba2f1ad08
Raka Hikmah Ramadhan <rakahikmah46@gmail.com>
2016-11-05 10:01:32 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,12 @@
 <?php 
 
 
-mysqli_connect("localhost","root","","project");
+$connect = mysqli_connect("localhost","root","","project");
 
- ?>
+if($connect){
+   // echo "Berhasil" ;
+   }else{
+      echo "gagal koneksi ada kesalahan ";
+ }
+
+?>
